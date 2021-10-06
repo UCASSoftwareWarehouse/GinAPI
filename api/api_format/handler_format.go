69dd70e3982a27875c52917a20fe37f29b92e407
yzchnb <34084331+yzchnb@users.noreply.github.com>
2021-10-06 22:21:49 +0800

    do some refactor

--- api/api_format/handler_format.go
+++ api/api_format/handler_format.go
@@ -3,28 +3,7 @@ package api_format
 import (
 	"GinAPI/err"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type SimpleJSONHandler func(*gin.Context) (*JSONRespFormat, *err.APIErr)
 type NormalHandler func(*gin.Context)
-
-func UnwrapSimpleJSONHandler(handler SimpleJSONHandler) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		c.Set("some_key", "some_value")
-		resp, e := handler(c)
-		if e != nil {
-			UnwrapErr(c, e)
-			return
-		}
-		if resp == nil {
-			c.JSON(http.StatusOK, SimpleOKResp(nil))
-			return
-		}
-		c.JSON(resp.Status, NewJSONResp(resp.Status, resp.Message, resp.Data))
-	}
-}
-
-func UnwrapErr(c *gin.Context, e *err.APIErr) {
-	c.JSON(e.Status, NewJSONResp(e.Status, e.Message, nil))
-}
--- api/api_format/wrapper.go
+++ api/api_format/wrapper.go
@@ -1,20 +1,48 @@
 package api_format
 
 import (
+	"GinAPI/err"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Wrap(handler interface{}) func(c *gin.Context) {
 	var target func(c *gin.Context)
 	switch handler.(type) {
 	case SimpleJSONHandler:
-		target = UnwrapSimpleJSONHandler(handler.(SimpleJSONHandler))
+		target = wrapSimpleJSONHandler(handler.(SimpleJSONHandler))
 		break
 	case NormalHandler:
-		target = handler.(func(ctx *gin.Context))
+		target = wrapNormalHandler(handler.(NormalHandler))
 		break
 	default:
 		panic("Unsupported Search API type")
 	}
 	return target
+}
+
+func wrapSimpleJSONHandler(handler SimpleJSONHandler) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.Set("some_key", "some_value")
+		resp, e := handler(c)
+		if e != nil {
+			UnwrapErr(c, e)
+			return
+		}
+		if resp == nil {
+			c.JSON(http.StatusOK, SimpleOKResp(nil))
+			return
+		}
+		c.JSON(resp.Status, NewJSONResp(resp.Status, resp.Message, resp.Data))
+	}
+}
+
+func wrapNormalHandler(handler NormalHandler) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		handler(c)
+	}
+}
+
+func UnwrapErr(c *gin.Context, e *err.APIErr) {
+	c.JSON(e.Status, NewJSONResp(e.Status, e.Message, nil))
 }
